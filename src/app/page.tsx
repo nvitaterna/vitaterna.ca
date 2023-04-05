0273082e45332da5cded0647dd55b09172b7b860
Nicolas Vitaterna <nvitaterna@gmail.com>
2023-04-05 02:08:42 +0000

    update blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,13 @@ const Home = async () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Home</h1>
+        <h1>Hi.</h1>
+        <p>
+          I'm Nicolas - currently working as a full-stack developer. Software
+          development is my passion. I also have a modest home lab where I host
+          multiple services that I use daily. I created this blog to share some
+          of the things I've learned.
+        </p>
         <h2>
           <Link href="/blog">Blog</Link>
         </h2>
